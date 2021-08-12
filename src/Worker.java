3b99d9758453362a0ddeab5b21ea59fe9a6d69cb
YuriyIM <vehpby@gmail.com>
2021-08-13 00:18:53 +0200

    First commit

---
+++ src/Worker.java
@@ -0,0 +1,28 @@
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskErrorListener errorCallback) {
+        this.errorCallback = errorCallback;
+    }
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is error!");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+}
